2946821648932543a8cd2e4059b71a5bb0c4fcf4
rickscole <ricksaubercole@gmail.com>
2023-11-28 12:57:19 -0500

    Create Standalone 4D Example with Fold Controller and Cycle Controller.py

---
+++ Mancala/Standalone 4D Example with Fold Controller and Cycle Controller.py
@@ -0,0 +1,52 @@
+
+
+def reset_relative_indices(activeSpot, boardLength, relativeBoardIndices):
+    index_reset = 0
+    leftovers = activeSpot % boardLength
+    for i in range(leftovers, boardLength):
+        relativeBoardIndices[i] = index_reset
+        index_reset+=1
+    for i in range(0, leftovers):
+        relativeBoardIndices[i] = index_reset
+        index_reset+=1
+    return relativeBoardIndices
+
+
+
+
+board = [9, 6, 4, 2] ## example of one that cycles immediately
+#board = [14, 4, 8, 2] ## example of one that folds
+input_board = board.copy()
+output_board = board.copy()
+input_board_list = [input_board.copy()]
+relative_board_indices = [0, 1, 2, 3]
+board_length = len(board)
+
+
+
+spot_on_board_is_empty = 0
+no_repeating_configurations = 0
+while spot_on_board_is_empty == 0 and no_repeating_configurations == 0:
+    first_position = input_board[0]
+    gini = int((first_position - first_position % board_length) / board_length)
+    pareto = first_position % board_length
+    input_board[0] = 0
+    for e, i in enumerate(output_board):
+        if pareto == 0:
+            output_board[e] = input_board[e] + gini
+        elif e == 0 or e > pareto:
+            output_board[e] = input_board[e] + gini
+        else:
+            output_board[e] = input_board[e] + gini + 1
+    relative_board_indices = reset_relative_indices(first_position, board_length, relative_board_indices)
+    for e, i in enumerate(output_board):
+        input_board[e] = output_board[relative_board_indices.index(e)]
+    if (output_board.index(0) if 0 in output_board else -1) >= 0:
+        spot_on_board_is_empty = 1
+    if input_board in input_board_list:
+        no_repeating_configurations = 1
+    else:
+        input_board_list.append(input_board.copy())
+   
+
+
