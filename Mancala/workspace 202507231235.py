d92e217e46725846f54347fcc2a9c5a8320c1bfe
rickscole <ricksaubercole@gmail.com>
2025-07-23 12:35:39 -0400

    Create workspace 202507231235.py

---
+++ Mancala/workspace 202507231235.py
@@ -0,0 +1,49 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  8 13:37:19 2025
+
+@author: rcole
+"""
+
+
+
+
+
+import board as brd
+import pandas as pd
+
+
+
+
+
+board = brd.Board([1] * 176)
+#board = brd.Board([50, 49, 49, 49, 49])
+#board = brd.Board([1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2])
+board_contents = board.get_board_contents()
+board_length = board.get_board_length()
+relative_board_indices = board.get_relative_board_indices()
+input_board_list = [board.get_board_contents()]
+input_board = board_contents.copy()
+output_board = board_contents.copy()
+contains_repeating_configurations = board.get_contains_repeating_configurations()
+
+## ITERATIVE ONE-DIMENSION REDISTRIBUTION
+while contains_repeating_configurations == 0:
+    board.distribute_dimensions_with_minimal_energy(input_board, board, output_board, relative_board_indices, input_board_list)
+    contains_repeating_configurations = board.get_contains_repeating_configurations()
+
+    
+if len(input_board_list[-1]) == 1:
+    contains_repeating_configurations = 0
+
+size = board.get_board_length()
+mass = board.get_mass()
+turns = board.get_turn()
+total_energy = board.get_total_energy()
+displaced_energy = board.get_displaced_energy()
+    
+
+
+
